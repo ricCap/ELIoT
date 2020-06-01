82c22fdb14bf80f48f7e3ce01969d7880dba121e
Riccardo Capraro <capraroriccardo@gmail.com>
2020-06-01 15:52:13 +0200

    Add persistent storage to k8s grafana and prometheus

--- k8s/setup-adm.sh
+++ k8s/setup-adm.sh
@@ -2,6 +2,8 @@
 
 set pipefail -euo
 
+export CALICO_VERSION="v3.14"
+
 # clean up previous configurations
 echo y | sudo kubeadm reset
 sudo rm -rf /etc/cni/net.d
@@ -13,4 +15,4 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/$CALICO_VERSION/manifests/calico.yaml
