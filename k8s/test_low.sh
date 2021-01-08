184a199c05956f50845c90fcd22393a50386713d
RicCapUnitn <riccardo.capraro@studenti.unitn.it>
2021-01-08 18:34:45 +0100

    Fix

--- k8s/test_low.sh
+++ k8s/test_low.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEVICES_TEST_YAML="$ELIOT_K8S_HOME/eliot/low.yaml"
+export DEVICES_TEST_YAML="$(pwd)/eliot/medium.yaml"
 export ELIOT_DEVICES=200
 source test_base.sh
 run
--- k8s/test_med.sh
+++ k8s/test_med.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEVICES_TEST_YAML="$ELIOT_K8S_HOME/eliot/medium.yaml"
+export DEVICES_TEST_YAML="$(pwd)/eliot/medium.yaml"
 export ELIOT_DEVICES=500
 source test_base.sh
 run
